b71bee71e2f5b5ee2a3c614761e933b4b6ed7dad
spoolean <jimjamsr@gmail.com>
2021-01-19 15:32:30 +0000

    Project Build

---
+++ ReciPeep/ReciPeep/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReciPeep.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
