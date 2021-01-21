e11fd0813b2c98f1eb5bc0aa9ecbe443f06f3d93
spoolean <jimjamsr@gmail.com>
2021-01-21 13:29:32 +0000

    Add Vue app Add button to add more fields Looks nice

--- ReciPeep/ReciPeep/wwwroot/js/site.js
+++ ReciPeep/ReciPeep/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+Vue.component('ingredient',{
+    template: '\
+        <div class="input-group pb-3">\
+            <input type="text" class="form-control" placeholder="Add an ingredient">\
+        </div>'
+})
+
+new Vue({
+    el: '#app',
+    data: {
+        count: 2,
+        ingredients: []
+    },
+    methods: {
+        addIngredient: function () {
+            this.count += 1;
+        }
+    }
+})
