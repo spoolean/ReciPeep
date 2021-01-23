4995e1d008f7d865a5041a15e4675240492a8a97
spoolean <jimjamsr@gmail.com>
2021-01-23 11:40:48 +0000

    adds HTTP post to unknown contoller where form inputs are added to an array of lists {ingredient: 'ingredient'}

--- ReciPeep/ReciPeep/wwwroot/js/site.js
+++ ReciPeep/ReciPeep/wwwroot/js/site.js
@@ -3,22 +3,65 @@
 
 // Write your JavaScript code.
 
-Vue.component('ingredient',{
-    template: '\
-        <div class="input-group pb-3">\
-            <input type="text" class="form-control" placeholder="Add an ingredient">\
-        </div>'
-})
+//Vue.component('ingredient',{
+
+//    props: ['ingredient'],
+//    data: function () {
+//        return {
+            
+//        }
+//    },
+//    template: '\
+//        <div class="input-group pb-3">\
+//            <input type="text" class="form-control" placeholder="Add an ingredient">\
+//        </div>'
+
+//})
 
 new Vue({
-    el: '#app',
-    data: {
-        count: 2,
-        ingredients: []
+    el: '#index',
+    data: function() {
+        return {
+            count: 0,
+            ingredients: [{ ingredient: '' }]
+        }
     },
     methods: {
         addIngredient: function () {
             this.count += 1;
+            this.ingredients.push({ ingredient: '' });
+        },
+
+        pushIngredients: function (data) {
+            Http.Post(`${window.location.origin}/whatevercontollerthishappenstobe`, data)
+                .then(data => console.log(data))
+                .catch(err => conso.log(err));
         }
     }
 })
+
+
+class Http {
+    async Get(url) {
+        var response = await fetch(url);
+
+        var data = await response.json();
+
+        return data;
+    }
+
+    async Post(url, data) {
+        var response = fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+
+        var result = await response.json();
+
+        return result;
+    }
+}
+
