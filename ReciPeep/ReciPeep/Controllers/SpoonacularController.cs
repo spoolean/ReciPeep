d34caf952b37b019ebe0866c1bef295b68571039
Isaac-cummings <43849557+Isaac-cummings@users.noreply.github.com>
2021-02-23 13:20:52 +0000

    Added comments

--- ReciPeep/ReciPeep/Controllers/SpoonacularController.cs
+++ ReciPeep/ReciPeep/Controllers/SpoonacularController.cs
@@ -11,12 +11,14 @@ namespace ReciPeep.Controllers
     [Route("Spoonacular")]
     public class SpoonacularController : Controller
     {
-
+        //Variable storing the key to the spoonacular account. This should be removed before making it public
         private string apiKey = "19276f6da3644e54981cc0d2dea5c426";
 
+        //Function for getting a random recipe
         [HttpGet("FeelingLucky")]
         public async Task<IActionResult> IngredientSearch()
         {
+            //variables for storing the url to call as well as the string to store the result
             HttpClient client = new HttpClient();
             string responseBody = @"{}";
             string url = "https://api.spoonacular.com/recipes/random?apiKey=" + apiKey + "&number=1";
@@ -37,13 +39,15 @@ namespace ReciPeep.Controllers
                 Console.WriteLine("Message :{0} ", e.Message);
             }
 
+            //convert the data to desired json types
             JObject ResponseObject = JObject.Parse(responseBody);
             JArray responseData = JArray.Parse(ResponseObject["recipes"].ToString());
             JArray skinnyRecipes = new JArray();
 
-
+            //loop through the list of recipes for extra info
             for (int i = 0; i < responseData.Count; i++)
             {
+                //call the extra info based on the recipe id
                 try
                 {
                     HttpResponseMessage response = await client.GetAsync(GetInfoURL(responseData[i]["id"].ToString()));
@@ -56,27 +60,29 @@ namespace ReciPeep.Controllers
                     Console.WriteLine("\nException Caught!");
                     Console.WriteLine("Message :{0} ", e.Message);
                 }
+                //create add to an array a cut down version of the recipe based on both sets of data
                 skinnyRecipes.Add(CutObject(responseData[i], JObject.Parse(responseBody)));
 
             }
-
-            JArray sortedSkinnyRecipes = new JArray(skinnyRecipes.OrderBy(obj => (string)obj["missedIngredientCount"]));
-
-            return Ok(sortedSkinnyRecipes.ToString());
+            return Ok(skinnyRecipes.ToString());
         }
 
 
         [HttpGet("GetRecipes/{ingredientsString}")]
         public async Task<IActionResult> IngredientSearch(string ingredientsString)
         {
+            //split up the ingredients string into a list
             string[] ingredients = ingredientsString.Split(",");
+            //store the number of wanted recipes 
             int numRecipes = 10;
 
+
+            //variables for storing the url to call as well as the string to store the result
             HttpClient client = new HttpClient();
             string responseBody = @"{}";
             string url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + apiKey + "&ingredients=";
 
-            //add ingredients to the search
+            //add ingredients to the search url by looping through the list of ingredients
             for (int i = 0; i < ingredients.Length; i++)
             {
                 url += ingredients[i];
@@ -127,8 +133,10 @@ namespace ReciPeep.Controllers
             return Ok(sortedSkinnyRecipes.ToString());
         }
 
+        //create a cutdown version of the recipe 
         private JObject CutObject(JToken recipe,JObject extraInfo)
         {
+            //create an object with an ID, title, image and url
             JObject outputRecipe = new JObject
             {
                 { "id", recipe["id"] },
@@ -136,20 +144,25 @@ namespace ReciPeep.Controllers
                 { "image", recipe["image"] },
                 { "sourceUrl", extraInfo["sourceUrl"] },
             };
+            //if it's from an ingredients search
             if(recipe["missedIngredientCount"] != null)
             {
+                //add the specific attributes
                 outputRecipe.Add("missedIngredientCount", recipe["missedIngredientCount"]);
                 outputRecipe.Add("missedIngredients", recipe["missedIngredients"]);
                 outputRecipe.Add("usedIngredients", recipe["usedIngredients"]);
             }
+            //if it's from I'm feeling lucky
             if(recipe["extendedIngredients"] != null)
             {
+                //just add the full list of ingredients
                 outputRecipe.Add("ingredients", recipe["extendedIngredients"]);
             }
             
             return outputRecipe;
         }
 
+        //create the url for getting more info from a recipe ID
         private string GetInfoURL(string recipeID)
         {
             string searchURL = "https://api.spoonacular.com/recipes/"+ recipeID +"/information?apiKey=" + apiKey + "&includeNutrition=false";
